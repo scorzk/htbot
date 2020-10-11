b42fd05b997bf310cb7ea0ab0adde202e2dd9a90
scorzk <38019336+scorzk@users.noreply.github.com>
2020-10-11 12:37:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api')
 
 const TOKEN='1320924854:AAG7AKbpKgi28xGwEpc1IVLJ6lIZJVST0Qg'
 
-conts bot = new TelegramBot(TOKEN,{polling: true})
+const bot = new TelegramBot(TOKEN,{polling: true})
 
 bot.on('message',msg=>{
 	bot.sendMessage(msg.chat.id,`Hello "Hi, ${msf.from.first_name}"`)
