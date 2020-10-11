d1d7e96ec888b8821a295743955bfb7d3493f4c6
scorzk <38019336+scorzk@users.noreply.github.com>
2020-10-11 18:09:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ const TOKEN='1320924854:AAG7AKbpKgi28xGwEpc1IVLJ6lIZJVST0Qg'
 
 const bot = new TelegramBot(TOKEN,{polling: true})
 
-var options = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{ text: 'Кнопка 1', callback_data: '1' }],
-      [{ text: 'Кнопка 2', callback_data: 'data 2' }],
-      [{ text: 'Кнопка 3', callback_data: 'text 3' }]
-    ]
-  })
-};
-
-bot.onText(/\/start_test/, function (msg, match) {
-  bot.sendMessage(msg.chat.id, 'Выберите любую кнопку:', options);
-});
+bot.on('message',msg=>{
+    bot.sendMessage(msg.chat.id,`Hello "Hi, ${msg.from.first_name}"`)
+})
