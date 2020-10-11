eb38b9e4842ef4b2e95d6783c842baa9879cfa83
scorzk <38019336+scorzk@users.noreply.github.com>
2020-10-11 18:15:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,22 @@ const bot = new TelegramBot(TOKEN,{polling: true})
 var options = {
   reply_markup: JSON.stringify({
     inline_keyboard: [
-      [{ text: 'Кнопка 1', callback_data: '1' }],
-      [{ text: 'Кнопка 2', callback_data: 'data 2' }],
-      [{ text: 'Кнопка 3', callback_data: 'text 3' }]
+      [{ text: 'Кнопка 1', callback_data: 'data1' }],
+      [{ text: 'Кнопка 2', callback_data: 'data2' }],
+      [{ text: 'Кнопка 3', callback_data: 'text3' }]
     ]
   })
 };
 
 bot.onText(/\/start_test/, function (msg, match) {
   bot.sendMessage(msg.chat.id, 'Выберите любую кнопку:', options);
+});
+bot.on("callback_query", function(data){
+    const chatId = msg.id;
+    console.log(data);
+    // Get the callback data specified
+   let callback_data = data.data
+   if(callback_data == "data1") {
+      bot.answerCallbackQuery(chatId,'hello'); 
+   }
 });
