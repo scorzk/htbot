79dd4c9e2e4062e613437cb3c3fc82237139e7f8
scorzk <38019336+scorzk@users.noreply.github.com>
2020-10-11 17:53:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ bot.onText(/\/start/, (msg) => {
     
 bot.sendMessage(msg.chat.id, "Welcome", {
 "reply_markup": {
+resize_keyboard: true,
+            one_time_keyboard: true,
     "keyboard": [["Sample text", "Second sample"],   ["Keyboard"], ["I'm robot"]]
     }
 });
